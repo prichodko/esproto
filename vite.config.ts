a297047098a4b8b6149eec02de635c9e1fb6b5cb
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-06-18 01:19:54 +0200

    add build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 
+import { dependencies } from './package.json'
+
+const external = [
+  ...Object.keys(dependencies || {}),
+  // ...Object.keys(peerDependencies || {}),
+]
+
 export default defineConfig({
   build: {
     lib: {
       entry: './src/index.ts',
       formats: ['es'],
+      fileName: 'index',
+    },
+
+    target: 'modules',
+    rollupOptions: {
+      external: external.map((name) => new RegExp(`^${name}(/.*)?`)),
     },
   },
+
   test: {},
 })
