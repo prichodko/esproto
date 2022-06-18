8833d76befadc7dd1e4b25d5778d5f56820eb9bd
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-06-18 17:00:43 +0200

    finish up encode/decode public api

--- src/decode.ts
+++ src/decode.ts
@@ -1,16 +1,19 @@
-import { createReader } from './reader'
+import { createReader, Reader } from './reader'
+import type { Message, DecodeFields } from './types'
 
 // todo: add flag to write default values for missing fields
-export function decode<T>(buffer: Uint8Array, fields: any): T {
-  const reader = createReader(buffer)
+export function decode<M extends Message>(fields: DecodeFields<M>) {
+  return (input: Uint8Array | Reader): M => {
+    const reader = input instanceof Uint8Array ? createReader(input) : input
 
-  const message: Record<string, unknown> = {}
+    const message = {} as M
 
-  while (reader.position < reader.limit) {
-    const tag = reader.tag()
-    const field = fields[tag]!
-    message[field.name] = field.decode(reader)
+    while (reader.position < reader.limit) {
+      const tag = reader.tag()
+      const field = fields[tag]!
+      message[field.name] = field.decode(reader)
+    }
+
+    return message as M
   }
-
-  return message as T
 }
--- src/encode.ts
+++ src/encode.ts
@@ -1,14 +1,17 @@
-import { createWriter } from './writer'
+import { createWriter, Writer } from './writer'
+import type { Message, EncodeFields } from './types'
 
 // todo: strict flag to throw errors for non-existant types or invariant
-export function encode<T>(message: T, fields) {
-  const writer = createWriter()
+export function encode<M extends Message>(fields: EncodeFields<M>) {
+  return (message: Partial<M>, _writer?: Writer): Uint8Array => {
+    const writer = _writer ?? createWriter()
 
-  for (const [key, value] of Object.entries(message)) {
-    const field = fields[key]!
-    writer.tag(field.tag, field.type)
-    field.encode(writer, value)
+    for (const [key, value] of Object.entries(message)) {
+      const field = fields[key as keyof M]
+      writer.tag(field.tag, field.encode.wireType)
+      field.encode(value, writer)
+    }
+
+    return writer.finish()
   }
-
-  return writer.finish()
 }
